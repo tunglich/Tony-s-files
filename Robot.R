409daeb107db3ecd6091268e6f526f33a30c5564
tunglich <32387103+tunglich@users.noreply.github.com>
2017-11-19 16:49:05 +0800

    Add files via upload

--- Robot.R
+++ Robot.R
@@ -17,9 +17,9 @@ library(ggplot2)
 windowsFont(BL = windowsFont('微軟正黑體'))
 
 
-#function: convert data type of char to double but keep Date as char, then tranform to xts object
+# function: convert data type of char to double but keep Date as char, then tranform to xts object
 
-csv2xts <- function(x){
+data2xts <- function(x){
   Date_temp <- x$Date
   x$Date <- NULL
   x <- sapply(x, as.numeric)
@@ -31,7 +31,7 @@ csv2xts <- function(x){
     
   }
 
-#loading data from Nomura funds
+# loading data from Nomura funds
 
 Nomura_Return_All <- read_excel("C:/Users/tungl/OneDrive/文件/GitHub/Nomura-TW-fund/Nomura_Return_All-1.xlsx"
                                 , sheet = 1, col_names = TRUE)
@@ -40,18 +40,18 @@ Nomura_Shape <- read_excel("C:/Users/tungl/OneDrive/文件/GitHub/Nomura-TW-fund/N
 index_all <- read_excel("C:/Users/tungl/OneDrive/文件/GitHub/Nomura-TW-fund/index-1.xlsx"
                                 , sheet = 1, col_names = TRUE)
 
-#call fuction to convert to xts object
+# call fuction to convert to xts object
 
-Nomura_Return_All <- csv2xts(Nomura_Return_All)
-Nomura_Shape <- csv2xts(Nomura_Shape)
-index_all <- csv2xts(index_all)
+Nomura_Return_All <- data2xts(Nomura_Return_All)
+Nomura_Shape <- data2xts(Nomura_Shape)
+index_all <- data2xts(index_all)
  
-#covert % return to numeric return
+# covert % return to numeric return
 
 Nomura_Return_All <- Nomura_Return_All / 100
 index_all <- index_all / 100
 
-#function: calculate cumulative return
+# function: calculate cumulative return
 
 cumReturn <- function(x, start_date = "2006-12-29", end_date = "2017-10-31") {
     period <- paste0(start_date, "/", end_date)
@@ -59,7 +59,7 @@ cumReturn <- function(x, start_date = "2006-12-29", end_date = "2017-10-31") {
     return(result)
 }
 
-#function: calculate annulized return
+# function: calculate annulized return
 
 annReturn <- function(x, start_date = "2006-12-29", end_date = "2017-10-31" ) {
     period <- paste0(start_date, "/", end_date)
@@ -67,4 +67,12 @@ annReturn <- function(x, start_date = "2006-12-29", end_date = "2017-10-31" ) {
     return(result)
 }
 
+# function: calculate periodical return
+# default frequency: "monthly"
+# alternative frequency: "weekly", quarterly", "yearly"
 
+periodReturn <- function(x, frequency = "monthly"){
+    period = paste0("apply.",frequency,"(x, cumReturn)")
+    result <- eval(parse(text = period))
+    return(result)
+}
